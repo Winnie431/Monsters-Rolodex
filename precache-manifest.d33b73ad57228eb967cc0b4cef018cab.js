0ff6b8aa6275d52e96b1f306f5635d930639daf0
Winnie431 <80514918+Winnie431@users.noreply.github.com>
2022-07-07 17:40:41 +0100

    Updates

---
+++ precache-manifest.d33b73ad57228eb967cc0b4cef018cab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "93e4e7deafdab7de59cf",
+    "url": "/monsters-rolodex/static/css/main.f26ba189.chunk.css"
+  },
+  {
+    "revision": "93e4e7deafdab7de59cf",
+    "url": "/monsters-rolodex/static/js/main.93e4e7de.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/monsters-rolodex/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "6a9ed8bb838c5d13005f",
+    "url": "/monsters-rolodex/static/js/2.6a9ed8bb.chunk.js"
+  },
+  {
+    "revision": "02c1b23ffcb7e58431d5",
+    "url": "/monsters-rolodex/static/js/runtime~main.02c1b23f.js"
+  },
+  {
+    "revision": "d187d84fe3f4f59e19022779e0a2efd6",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
